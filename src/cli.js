175aee4cbc23bf8714252d92b6d20238abf2db94
boriy107 <borispoligraph@gmail.com>
2022-09-15 12:09:01 +0300

    install eslint

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,20 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
-    printWelcome();
-    let name = askName();
-    printHello(name);
-}
-
 const printWelcome = () => {
-    console.log('Welcome to the Brain Games!');
-}
+  console.log('Welcome to the Brain Games!');
+};
 
 const askName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return userName;
-}
+  const userName = readlineSync.question('May I have your name? ');
+  return userName;
+};
 
 const printHello = (name) => {
-    console.log('Hello, ' + name + '!');
-}
+  console.log(`Hello, ${name}!`);
+};
+
+export default () => {
+  printWelcome();
+  const name = askName();
+  printHello(name);
+};
