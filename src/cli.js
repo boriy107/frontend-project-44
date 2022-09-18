0d8a7b288bea2aab8a4d71fdf281738596b7b666
boriy107 <borispoligraph@gmail.com>
2022-09-18 20:05:11 +0300

    rewrite cli.js logic

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const printWelcome = () => {
-  console.log('Welcome to the Brain Games!');
-};
-
-const askName = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  return userName;
-};
-
-const printHello = (name) => {
-  console.log(`Hello, ${name}!`);
-};
-
 export default () => {
-  printWelcome();
-  const name = askName();
-  printHello(name);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
